466f501cc1536bbebeca295d3aad9a318bf3d487
makasinui <mks.shi@ya.ru>
2025-07-03 22:11:41 +0300

    fix(habitStore): return habit, add mock habit

--- stores/habit.ts
+++ stores/habit.ts
@@ -1,6 +1,9 @@
 import type { Habit } from "~/types/habit";
 
 export const useHabitStore = defineStore('habit', () => {
-    const habits = ref<Habit[]>([]);
-    
+    const habits = ref<Habit[]>([{id: 1, title: 'asd', type: 'daily', goal: 'asd', startDate: Date.now()}]);
+
+    return {
+        habits
+    }
 });
