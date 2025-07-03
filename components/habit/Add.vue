bff62d2d479681cd84ff0816f8f9ef806917c7ca
makasinui <mks.shi@ya.ru>
2025-07-03 22:28:50 +0300

    feat(habit): ui for add new habit

---
+++ components/habit/Add.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="add-habit">
+        <Icon name="fluent-color:add-circle-24" size="75" />
+    </div>
+</template>
+
+<style lang="scss">
+@use '/styles/mixins' as *;
+
+.add-habit {
+    position: fixed;
+    bottom: toRem(30);
+    right: toRem(50);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
         <HabitCards />
+        <HabitAdd />
     </div>
 </template>
 
