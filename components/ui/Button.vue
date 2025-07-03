0f47555dba140abfdd53876bb9509409fd0a6356
makasinui <mks.shi@ya.ru>
2025-07-03 23:39:09 +0300

    feat(ui): button component

---
+++ components/ui/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="button">
+        <slot/>
+    </button>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss">
+@use '~/styles/variables' as *;
+@use '~/styles/mixins' as *;
+
+.button {
+    padding: toRem(15) 0;
+    font-size: toRem(14);
+    color: #fff;
+    background: $main-color;
+    border: 0;
+    border-radius: toRem(8);
+    font-weight: 600;
+    cursor: pointer;
+    transition: 0.3s background-color;
+
+    &:hover {
+        background: $main-color-hover;
+    }
+}
+</style>
