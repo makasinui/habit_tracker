bf78f38e71c47ffddc419c31fd94dd0a9910a3a3
makasinui <mks.shi@ya.ru>
2025-07-03 22:45:24 +0300

    feat(ui): modal component

---
+++ components/ui/Modal.vue
@@ -0,0 +1,78 @@
+<template>
+    <Teleport
+        v-if="modelValue"
+        to="body"
+    >
+        <article class="modal">
+            <div class="modal__wrapper">
+                <div class="modal__content">
+                    <header class="modal__header">
+                        <h3 class="modal__header-title">{{ title }}</h3>
+                        <Icon
+                            name="line-md:close"
+                            size="20"
+                            @click="modelValue = false"
+                        />
+                    </header>
+                    <div class="modal__body">
+                        <slot />
+                    </div>
+                </div>
+            </div>
+        </article>
+    </Teleport>
+</template>
+
+<script lang="ts" setup>
+interface ModalProps {
+    title: string;
+}
+
+defineProps<ModalProps>();
+const modelValue = defineModel<boolean>({ default: false });
+</script>
+
+<style lang="scss">
+@use '/styles/mixins' as *;
+
+.modal {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background: #cbcbcb;
+    opacity: 0.95;
+    top: 0;
+    z-index: 100000;
+
+    &__wrapper {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        padding: 0 toRem(22);
+        width: 100%;
+    }
+
+    &__content {
+        background: #FCFCFF;
+        padding: toRem(15);
+        border-radius: toRem(6);
+    }
+
+    &__header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-bottom: 1px solid #E0E0E0;
+        padding-bottom: toRem(10);
+
+        &-title {
+            font-size: toRem(21);
+        }
+    }
+
+    &__body {
+        margin-top: toRem(22);
+    }
+}
+</style>
