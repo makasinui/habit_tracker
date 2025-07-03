90c087277045bee70cdf3f9ce51f18c40d1e7491
makasinui <mks.shi@ya.ru>
2025-07-03 23:02:56 +0300

    feat(ui): create form field component

---
+++ components/ui/FormField.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="form-field">
+        <span class="form-field__label">{{ label }}</span>
+        <UiInput v-model="modelValue" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+interface FormFieldProps {
+    label: string;
+}
+
+defineProps<FormFieldProps>();
+const modelValue = defineModel<string>('');
+</script>
+
+<style lang="scss">
+@use '/styles/mixins' as *;
+
+.form-field {
+    display: flex;
+    flex-direction: column;
+    gap: toRem(8);
+
+    &__label {
+        font-size: toRem(14);
+    }
+}
+</style>
