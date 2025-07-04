41280e2c8d5adcea4be6121cfc0e87b175f185a3
makasinui <mks.shi@ya.ru>
2025-07-04 12:17:09 +0300

    fix(habit): delete mock habits from store

--- stores/habit.ts
+++ stores/habit.ts
@@ -1,10 +1,7 @@
 import type { Habit, HabitForm } from '~/types';
 
 export const useHabitStore = defineStore('habit', () => {
-    const habits = ref<Habit[]>([
-        { id: 1, title: 'asd', type: 'daily', goal: 'asd', startDate: Date.now() },
-        { id: 2, title: 'asd', type: 'daily', goal: 'asd', startDate: Date.now() },
-    ]);
+    const habits = ref<Habit[]>([]);
 
     const isOpenCreateModal = ref(false);
 
