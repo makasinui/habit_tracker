48154b262a1cfa9197fbb8da354bb73a37428822
makasinui <mks.shi@ya.ru>
2025-07-09 14:19:32 +0300

    feat(habit): save habits in local storage

--- stores/habit.ts
+++ stores/habit.ts
@@ -11,10 +11,12 @@ export const useHabitStore = defineStore('habit', () => {
             ...form,
             period,
             type,
-            id: Date.now()
+            id: Date.now(),
         });
-    }
-    
+
+        saveInStorage();
+    };
+
     const updateHabit = (form: Required<HabitForm>) => {
         const period = form.period.id;
         const type = form.type.id;
@@ -22,27 +24,42 @@ export const useHabitStore = defineStore('habit', () => {
         const habit = {
             ...form,
             period,
-            type
+            type,
         };
 
         const habitIdx = habits.value.findIndex((hab) => hab.id === habit.id);
 
-        if(habitIdx !== -1) {
-            habits.value.splice(habitIdx, 1, habit)
+        if (habitIdx !== -1) {
+            habits.value.splice(habitIdx, 1, habit);
+            saveInStorage();
         }
-    }
+    };
 
     const deleteHabit = (habitId: number) => {
         const habitIdx = habits.value.findIndex((hab) => hab.id === habitId);
-        if(habitIdx !== -1) {
+        
+        if (habitIdx !== -1) {
             habits.value.splice(habitIdx, 1);
+            saveInStorage();
         }
+    };
+
+    const saveInStorage = () => {
+        localStorage.setItem('habits', JSON.stringify(habits.value));
     }
+
+    onMounted(() => {
+        const localHabits = localStorage.getItem('habits');
+        console.log(localHabits)
+        if(localHabits) {
+            habits.value = JSON.parse(localHabits);
+        }
+    })
 
     return {
         habits,
         addHabit,
         updateHabit,
-        deleteHabit
+        deleteHabit,
     };
 });
