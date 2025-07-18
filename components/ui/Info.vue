8a55bbc067a56fff432c09406164fd303e666e54
makasinui <mks.shi@ya.ru>
2025-07-18 17:54:32 +0300

    feat(ui): component for info

---
+++ components/ui/Info.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="info">
+        <div class="info__title">
+            {{ title }}
+        </div>
+        <div class="info__value">
+            {{ value }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+interface InfoProps {
+    title: string
+    value: string | number | null
+}
+
+defineProps<InfoProps>();
+</script>
+
+<style lang="scss">
+@use '/styles/mixins' as *;
+
+.info {
+    display: flex;
+    justify-content: space-between;
+
+    &__title, &__value {
+        font-size: toRem(14);
+    }
+}
+</style>
